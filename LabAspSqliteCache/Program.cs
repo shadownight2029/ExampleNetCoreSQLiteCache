37e5f80a74e2bd7b824be25fea0d34de32dcece2
Arnon Potipussa <arnonpotipussa@gmail.com>
2020-01-08 12:49:58 +0700

    first commit

---
+++ LabAspSqliteCache/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NeoSmart.Caching.Sqlite;
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace LabAspSqliteCache
+{
+    class Program
+    {
+        IDistributedCache cc;
+
+        static void Main(string[] args)
+        {
+            string helloText = string.Empty;
+            string cachePath = System.IO.Path.Combine(Environment.CurrentDirectory, "cache.db");
+            SqliteCacheOptions Configuration = new SqliteCacheOptions()
+            {
+                MemoryOnly = false,
+                CachePath = cachePath,
+            };
+
+            var logger = new Logger<SqliteCache>(new LoggerFactory());
+            var cache = new SqliteCache(Configuration, logger);
+            var bytes = cache.Get("hello");
+            if (bytes == null)
+            {
+                cache.Set("hello", Encoding.ASCII.GetBytes("hello"), new DistributedCacheEntryOptions()
+                {
+                    AbsoluteExpiration = DateTimeOffset.UtcNow.AddDays(1)
+                });
+
+                helloText = Encoding.ASCII.GetString(cache.Get("hello")) + " from set";
+
+            }
+            else
+            {
+                helloText = Encoding.ASCII.GetString(cache.Get("hello")) + " from cache";
+            }
+
+            Console.WriteLine(helloText);
+            Console.ReadLine();
+        }
+    }
+}
